eb4c4cdbe4deb5e417e398681f326ed5624668ba
pnkjsajwan <pankz.sajwan@outlook.com>
2020-05-05 15:42:54 +0530

    making frist form

---
+++ app/src/main/java/com/corona/lockdown/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.corona.lockdown
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
