162c4ce271fa61fe402da05bca6bd9d46b4efdef
pnkjsajwan <pankz.sajwan@outlook.com>
2020-05-11 21:39:31 +0530

    frist code with kotlin

--- app/src/main/java/com/corona/lockdown/MainActivity.kt
+++ app/src/main/java/com/corona/lockdown/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.corona.lockdown
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_show_toast.setOnClickListener {
+            Log.w("MainActivity", "Button was clicked")
+            Toast.makeText(this, "Welcome Pankaj Sajwan", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
