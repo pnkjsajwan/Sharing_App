b08d918574df46d7253193979e0082928ce1880e
pnkjsajwan <pankz.sajwan@outlook.com>
2020-05-17 16:45:00 +0530

    working on third activity

--- app/src/main/java/com/corona/lockdown/MainActivity.kt
+++ app/src/main/java/com/corona/lockdown/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.corona.lockdown
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,10 +11,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_show_toast.setOnClickListener {
-            Log.w("MainActivity", "Button was clicked")
-            Toast.makeText(this, "Welcome Pankaj Sajwan", Toast.LENGTH_SHORT).show()
-
+//       button_show_toast.setOnClickListener {
+//           Log.i("MainActivity","button is pressed")
+//           Toast.makeText(this,"button is pressed",Toast.LENGTH_LONG).show()
+//       }
+        button_submit.setOnClickListener {
+            val msg1: String = edit_view_user_frist_name.text.toString()
+            val msg2: String = edit_view_user_last_name.text.toString()
+            val msg3: String = edit_view_user_mobile_no.text.toString()
+            Toast.makeText(this, "form fill sucessfully", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, Secound_activity::class.java)
+            intent.putExtra("user_frist_name", msg1)
+            intent.putExtra("user_secound_name", msg2)
+            intent.putExtra("user_mobile_no", msg3)
+            startActivity(intent)
         }
+
     }
 }
